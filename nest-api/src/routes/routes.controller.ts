8f9d9197e98c8658aff305e175e6d10f52bd542d
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2023-02-13 02:25:46 -0300

    refactoring

--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -6,14 +6,29 @@ import {
     Patch,
     Param,
     Delete,
+    Inject,
+    OnModuleInit,
 } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka } from '@nestjs/microservices/client';
+import { Producer } from 'kafkajs';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('routes')
-export class RoutesController {
-    constructor(private readonly routesService: RoutesService) {}
+export class RoutesController implements OnModuleInit {
+    private kafkaProducer: Producer;
+
+    constructor(
+        private readonly routesService: RoutesService,
+        @Inject('KAFKA_SERVICE')
+        private readonly kafkaClient: ClientKafka,
+    ) {}
+
+    async onModuleInit() {
+        this.kafkaProducer = await this.kafkaClient.connect();
+    }
 
     @Post()
     create(@Body() createRouteDto: CreateRouteDto) {
@@ -38,5 +53,33 @@ export class RoutesController {
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.routesService.remove(+id);
+    }
+
+    @Get(':id/start')
+    startRoute(@Param('id') id: string) {
+        this.kafkaProducer.send({
+            topic: 'routeNewDirection',
+            messages: [
+                {
+                    key: 'routeNewDirection',
+                    value: JSON.stringify({ routeId: id, clientId: '' }),
+                },
+            ],
+        });
+    }
+
+    @MessagePattern('routeNewPosition')
+    consumeNewPosition(
+        @Payload()
+        message: {
+            value: {
+                routeId: string;
+                clientId: string;
+                position: [number, number];
+                finished: boolean;
+            };
+        },
+    ) {
+        console.log(message.value);
     }
 }
